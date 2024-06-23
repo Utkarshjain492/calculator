313c425acdecac77752d69bcc5b7a6912278b8c2
Utkarshjain492 <118668309+Utkarshjain492@users.noreply.github.com>
2024-06-23 23:16:19 +0530

    changes

--- normalCalculator.js
+++ normalCalculator.js
@@ -1,11 +1,14 @@
+const head = document.querySelector('#heading');
 const input1 = document.querySelector('#ip1');
 const input2 = document.querySelector('#ip2');
-const head = document.querySelector('#heading');
+const opr = document.querySelector('.operators');
+const equ = document.querySelector('.equal');
 
 const add = document.querySelector('#add');
 const sub = document.querySelector('#sub');
 const mul = document.querySelector('#mul');
 const div = document.querySelector('#div');
+const reset = document.querySelector('#reset');
 
 const result = document.querySelector('#result');
 
@@ -27,4 +30,10 @@ mul.addEventListener('click', () => {
 div.addEventListener('click', () => {
     res = Number(input1.value) / Number(input2.value);
     result.innerHTML = res;
+});
+
+reset.addEventListener('click', () => {
+    input1.value = ('');
+    input2.value = ('');
+    result.innerHTML = ('');
 });
